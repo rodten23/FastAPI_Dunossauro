a0db10ecbf00f8386f5e51d10b463869e22b65ae
Rodrigo Alves Tenório <rodten23@gmail.com>
2025-12-12 20:19:41 -0300

    :sparkles: feat: Alterações para que a engine seja criada de maneira assíncrona, assim como a sessão do ORM.

--- fastapi_dunossauro/database.py
+++ fastapi_dunossauro/database.py
@@ -1,11 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from fastapi_dunossauro.settings import Settings
 
-engine = create_engine(Settings().DATABASE_URL)
+# create_async_engine cria a engine de conexão assíncrona com o banco de dados
+# o qual é chamado com DATABASE_URL='sqlite+aiosqlite:///database.db' no .env
+engine = create_async_engine(Settings().DATABASE_URL)
 
 
-def get_session():
-    with Session(engine) as session:
+# AsyncSession gerencia a sessão de banco de dados de forma assíncrona.
+async def get_session():
+    async with AsyncSession(engine, expire_on_commit=False) as session:
         yield session
+
+# O expire_on_commit=False informa ao SQLAlchemy para não expirar os objetos
+# carregados após um commit. O comportamento padrão do SQLAlchemy é expirar o
+# cache de objetos após um commit, o que pode causar problemas em operações
+# assíncronas, pois o objeto pode ser descartado enquanto estamos aguardando
+# em outra corrotina.
